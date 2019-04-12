f6e1ff8e00d1b35eecf84694417c1a181d5fc8d4
Sunil Kumar <sunilssaharan@gmail.com>
2019-04-13 02:21:14 +0530

    at completion

--- src/binary_search.cpp
+++ src/binary_search.cpp
@@ -1,15 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h> 
 #include "file_manager.h"
 #include "errors.h"
 #include<cstring>
 
 using namespace std;
+int verbose = 0;
 
 int main(int argc, char *argv[]){
-	cout << "No. of arguments:: "<<argc << endl;
+	if(verbose)
+		cout << "No. of arguments:: "<<argc << endl;
 	if(argc != 3){
-		cout << "Argument miss match"<< endl;
-		cout << "usage :: ./binary_search <file> <int>\n";
+		if(verbose){
+			cout << "Argument miss match"<< endl;
+			cout << "usage :: ./binary_search <file> <int>\n";
+		}
 		return 1;
 	}
 
@@ -20,19 +24,51 @@ int main(int argc, char *argv[]){
 	// Open file from input
 	FileHandler fh = fm.OpenFile(argv[1]);
 
+	//  intialize variables 
+	int start =0,end,searchInt;
+	searchInt = atoi(argv[2]);
+
 	// No. of pages in file
-	cout << fh.LastPage().GetPageNum() <<endl;
+	end =  fh.LastPage().GetPageNum();
 
-	PageHandler ph = fh.FirstPage();
+	PageHandler ph ;
 
-	int pageNum = -1;
+	int pageNum = -1,num;
+	char* data;
+	bool found = false;
+	int intPage=-1,intEntryNum=-1;
 	// Lets inspect the data here
-	while(true){
-		pageNum = ph.GetPageNum();
-		cout << "Page Number :: "<< pageNum << endl;
-		ph = fh.NextPage(pageNum);
+	while(start <= end && !found){
+		ph = fh.PageAt((start+end)/2);
+		data = ph.GetData();
+
+		// check for firt number 
+		memcpy (&num, &data[0], sizeof(int));
+		if(num > searchInt){
+			end = (start+end)/2 - 1;
+			continue;
+		}
+
+
+		for(int i=0;i<(PAGE_CONTENT_SIZE/(sizeof(int)));i++){
+			memcpy (&num, &data[i*4], sizeof(int));
+			// cout << num << endl;
+			if(num == searchInt){
+				found = true;
+				intPage = (start+end)/2 +1;
+				intEntryNum = i+1;
+				break;
+			}
+
+			if(num==INT_MIN)
+				break;
+		}
+
+
+		start++;
 	}
 
+	cout << "("<<intPage<<","<<intEntryNum<<")"<<endl;
 
 	return 0;
 }
